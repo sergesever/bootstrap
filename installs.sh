97b7036849c01dd513969760b33bcab242a2acc8
sergesever <sergesever@gmail.com>
2015-12-15 00:09:20 +0200

    UPDATE installs.sh

--- installs.sh
+++ installs.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-#sudo apt-add-repository ppa:diodon-team/stable
 sudo apt-get update
 sudo apt-get install vim vim-gnome pidgin pidgin-otr krusader konsole thunderbird mc chromium-browser clamtk htop elinks curl git  gitk git-gui
 sudo apt-get install build-essential compizconfig-settings-manager vagrant exuberant-ctags
+
+sudo apt-get install sysinfo hardinfo
+sudo apt-get install midori epiphany-browser
+
+#sudo dpkg --get-selections
+#sudo apt-get install gimp mplayer
+
+#extra tools
+#sudo apt-get install sqlitebrowser pac
+
 #gcc
 
-#truecrypt pear php-dev unrar diodon
+#sudo apt-add-repository ppa:diodon-team/stable && apt-get install diodon
+#pear php-dev unrar
 
+#AMP
 #apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql phpmyadmin
 
+#nginx
+
 #php-pear && sudo pear install doc.php.net/pman
+
+#tops
 #apachetop atop mytop iotop ncdu nmap ntop
 
 #gksudo gedit /etc/mysql/my.cnf #set bind-address = 127.0.0.1
@@ -27,8 +42,4 @@ sudo apt-get install build-essential compizconfig-settings-manager vagrant exube
 #ps aux 
 #nice /renice for priority
 
-#sudo dpkg --get-selections
-#sudo apt-get install gimp mplayer pac
 
-sudo apt-get install sysinfo hardinfo
-sudo apt-get install midori epiphany-browser
