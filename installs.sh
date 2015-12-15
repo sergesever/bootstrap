e972927243f353d8f49f41a715bf881281aefbdf
sergesever <sergesever@gmail.com>
2015-12-15 12:15:23 +0200

    UPDATE installs.sh

--- installs.sh
+++ installs.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install vim vim-gnome pidgin pidgin-otr krusader konsole thunderbird mc chromium-browser clamtk htop elinks curl git  gitk git-gui
-sudo apt-get install build-essential compizconfig-settings-manager vagrant exuberant-ctags
 
+#basic
+sudo apt-get install vim vim-gnome krusader konsole mc chromium-browser clamtk htop elinks curl git  gitk git-gui
+
+#web
+sudo apt-get pidgin pidgin-otr thunderbird  midori epiphany-browser
+
+#tweaks
+sudo apt-get install compizconfig-settings-manager 
+
+#dev
+sudo apt-get install build-essential vagrant exuberant-ctags
+
+#system
 sudo apt-get install sysinfo hardinfo
-sudo apt-get install midori epiphany-browser
 
-#sudo dpkg --get-selections
+#db basic
+sudo apt-get install sqlite3 sqlitebrowser
+
+#mmedia
 #sudo apt-get install gimp mplayer
 
 #extra tools
-#sudo apt-get install sqlitebrowser pac
 
-#gcc
+#sudo dpkg --get-selections
+#sudo apt-get install gcc
 
 #sudo apt-add-repository ppa:diodon-team/stable && apt-get install diodon
-#pear php-dev unrar
+#sudo apt-get install pear php-dev unrar
 
-#AMP
+#AMP stack
 #apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql phpmyadmin
 
 #nginx
@@ -26,20 +39,19 @@ sudo apt-get install midori epiphany-browser
 #php-pear && sudo pear install doc.php.net/pman
 
 #tops
-#apachetop atop mytop iotop ncdu nmap ntop
+#apachetop atop mytop iotop ncdu nmap ntoa htop
 
 #gksudo gedit /etc/mysql/my.cnf #set bind-address = 127.0.0.1
 #mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('yourpassword')"
 #gksudo gedit /etc/php5/apache2/php.ini #uncomment extension=mysql.so
 #sudo /etc/init.d/apache2 restart
 
-#useful
+
 #uptime
 #cat /proc/cpuinfo | grep 'cpu cores'
-#top htop atop mytop apachetop #zombie prcesses
 #free -m
 #vfu #file manager
 #ps aux 
-#nice /renice for priority
+#nice #renice for priority
 
 
