92fb3cb982aa8e8dd78df19e8808625447997ed8
sergesever <sergesever@gmail.com>
2016-04-29 12:46:23 +0300

    UPDATE vimrc, installs.sh

--- installs.sh
+++ installs.sh
@@ -1,57 +1,84 @@
 #!/bin/bash
 
-sudo apt-get update
+#sudo apt-get update
 
 #basic
-sudo apt-get install vim vim-gnome krusader konsole mc chromium-browser clamtk htop elinks curl git  gitk git-gui
+#sudo apt-get install vim mc htop elinks curl git  
+
+#gui utils
+#sudo apt-get install krusader konsole chromium-browser gitk git-gui clamtk
 
 #web
-sudo apt-get pidgin pidgin-otr thunderbird  midori epiphany-browser
+#sudo apt-get pidgin pidgin-otr thunderbird  midori epiphany-browser
 
 #tweaks
-sudo apt-get install compizconfig-settings-manager 
+#sudo apt-get install compizconfig-settings-manager 
 
 #dev
-sudo apt-get install build-essential vagrant exuberant-ctags
+#sudo apt-get install build-essential vagrant exuberant-ctags
 
 #system
-sudo apt-get install sysinfo hardinfo
+#sudo apt-get install sysinfo hardinfo
 
 #db basic
-sudo apt-get install sqlite3 sqlitebrowser
+#sudo apt-get install sqlite3 sqlitebrowser
 
 #mmedia
 #sudo apt-get install gimp mplayer
 
+
+
 #extra tools
 
-#sudo dpkg --get-selections
-#sudo apt-get install gcc
+#python
+#sudo apt-get install python python-pip
+#wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh | sh
 
+#LAMP/LEMP stack
+#sudo apt-get install mysql-server mysql-client php5-mysql php5-mcrypt php5-cli phpmyadmin
+#apt-cache search php5
+
+#nginx+php-fmp
+#sudo apt-get install nginx php5-fpm php5-mysql php5-curl php5-gd php-mail
+
+#apache2
+#sudo apt-get install apache2 libapache2-mod-php5 libapache2-mod-auth-mysql  php5-mcrypt php5-cli phpmyadmin
+#sudo mysql_install_db && sudo mysql_secure_installation
+
+#docker
+#sudo curl -fsSL https://get.docker.com/ | sh
+#sudo pip install docker-compose
+
+#diodon
 #sudo apt-add-repository ppa:diodon-team/stable && apt-get install diodon
-#sudo apt-get install pear php-dev unrar
 
-#AMP stack
-#apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql phpmyadmin
+#for vim
+#sudo apt-get install php-pear
+#sudo pear install doc.php.net/pman
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+#:PluginInstall
 
-#nginx
-
-#php-pear && sudo pear install doc.php.net/pman
 
 #tops
-#apachetop atop mytop iotop ncdu nmap ntoa htop
+#sudo apt-get install apachetop atop mytop iotop ncdu nmap ntoa htop
+
+
+
+#misc minor operations
+
+#sudo dpkg -l
 
 #gksudo gedit /etc/mysql/my.cnf #set bind-address = 127.0.0.1
 #mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('yourpassword')"
 #gksudo gedit /etc/php5/apache2/php.ini #uncomment extension=mysql.so
-#sudo /etc/init.d/apache2 restart
 
-
+#optional
 #uptime
 #cat /proc/cpuinfo | grep 'cpu cores'
 #free -m
-#vfu #file manager
+#vfu
 #ps aux 
-#nice #renice for priority
+#nice #renice 
 
-
+#cp .gitconfig ~/
+#cp .vimrc /etc/vim/vimrc.local
